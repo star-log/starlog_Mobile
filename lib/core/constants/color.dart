0e05c061ce5079dc30aaca6e6c3845f0738a1953
leejia0324 <hx.un0707@gmail.com>
2026-06-10 23:05:41 +0900

    feat: Color 시스템 추가

---
+++ lib/core/constants/color.dart
@@ -0,0 +1,43 @@
+import 'dart:ui';
+
+class StarlogColor {
+  static const star = Color(0x00FFC30F);
+  static const black = Color(0xFF393939);
+  static const white = Color(0xFFFFFFFF);
+  static const grayBox = Color(0xFFF8F8F8);
+  static const grayText = Color(0xFFA2A2A2);
+}
+
+class LuckyColor {
+  static const money = Color(0xFFFFDF94);
+  static const moneyBorder = Color(0xFFDEC895);
+  static const love = Color(0xFFFFC4D5);
+  static const loveBorder = Color(0xFFEFB1C3);
+  static const work = Color(0xFF9BCCFF);
+  static const workBorder = Color(0xFF8BB3DE);
+  static const health = Color(0xFFABE6B1);
+  static const healthBorder = Color(0xFF90CE96);
+  static const key = Color(0xFFC7C7C7);
+}
+
+class RankingColor {
+  static const first = Color(0xFFFFD793);
+  static const second = Color(0xFFD5D2D2);
+  static const third = Color(0xFFE0CAA1);
+  static const other = Color(0xFFD3D7FF);
+}
+
+class ZodiacColor {
+  static const gemini = Color(0xFFFFCF99);
+  static const aquarius = Color(0xFF99C5FF);
+  static const libra = Color(0xFFFFAF99);
+  static const aries = Color(0xFFD3C4AA);
+  static const taurus = Color(0xFFFFBE56);
+  static const cancer = Color(0xFFFF89C6);
+  static const leo = Color(0xFFFFB16E);
+  static const scorpio = Color(0xFFCB8CFF);
+  static const sagittarius = Color(0xFF97F0EC);
+  static const capricorn = Color(0xFFACE5B4);
+  static const pisces = Color(0xFF83DCFF);
+  static const virgo = Color(0xFFFFB3DD);
+}
