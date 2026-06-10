c34695f291cd6be2488e417ba220dc18017ddbcd
leejia0324 <hx.un0707@gmail.com>
2026-06-10 22:17:30 +0900

    refactor: text style 수정

--- lib/core/constants/text_style.dart
+++ lib/core/constants/text_style.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:starlog_mobile/core/constants/color.dart';
 
 abstract final class StarlogTextStyle {
   static TextStyle title1 = defaultTextStyle.copyWith(
     fontSize: 28,
     fontWeight: FontWeight.w900,
-  );
-
-  static TextStyle title2 = defaultTextStyle.copyWith(
-    fontSize: 26,
-    fontWeight: FontWeight.w700,
   );
 
   static TextStyle subTitle1 = defaultTextStyle.copyWith(
@@ -35,8 +31,15 @@ abstract final class StarlogTextStyle {
     fontSize: 12,
     fontWeight: FontWeight.w400,
   );
+
+  static TextStyle splash = defaultTextStyle.copyWith(
+    fontSize: 26,
+    fontFamily: 'MaruBuri',
+    fontWeight: FontWeight.w400,
+  );
 }
 
 const TextStyle defaultTextStyle = TextStyle(
   fontFamily: 'Pretendard',
+  color: StarlogColor.white,
 );
