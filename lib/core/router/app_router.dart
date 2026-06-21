92bed91f8d0418743e8e3781b4d77e060e3d8a1c
leejia0324 <hx.un0707@gmail.com>
2026-06-21 14:17:37 +0900

    feat: router path에 onBoarding 추가

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:starlog_mobile/features/main/presentation/view/main_view.dart';
+import 'package:starlog_mobile/features/on_boarding/presentation/on_boarding_view.dart';
 import 'package:starlog_mobile/features/splash/presentation/splash.dart';
 
 final GoRouter appRouter = GoRouter(
@@ -16,9 +17,9 @@ final GoRouter appRouter = GoRouter(
       ),
     ),
     GoRoute(
-      path: '/',
+      path: '/onBoarding',
       pageBuilder: (context, state) => CustomTransitionPage(
-        child: const StarlogMain(),
+        child: const OnBoardingView(),
         transitionsBuilder: (context, animation, secondaryAnimation, child) =>
             FadeTransition(opacity: animation, child: child),
         transitionDuration: const Duration(milliseconds: 100),
