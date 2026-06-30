89509d143748bdbcbd170e22bc1c85a7dbe87f00
leejia0324 <hx.un0707@gmail.com>
2026-06-30 22:45:37 +0900

    fix: 생년월일 별자리 변경 로직 잘못된 날짜 수정

--- lib/core/enums/zodiac.dart
+++ lib/core/enums/zodiac.dart
@@ -83,7 +83,7 @@ extension ZodiacExtension on Zodiac {
 
   static Zodiac fromBirthday(int month, int day) {
     return switch ((month, day)) {
-      (1, <= 19) => Zodiac.capricorn,
+      (12, >= 25) || (1, <= 19) => Zodiac.capricorn,
       (1, >= 20) || (2, <= 18) => Zodiac.aquarius,
       (2, >= 19) || (3, <= 20) => Zodiac.pisces,
       (3, >= 21) || (4, <= 19) => Zodiac.aries,
@@ -91,10 +91,10 @@ extension ZodiacExtension on Zodiac {
       (5, >= 21) || (6, <= 21) => Zodiac.gemini,
       (6, >= 22) || (7, <= 22) => Zodiac.cancer,
       (7, >= 23) || (8, <= 22) => Zodiac.leo,
-      (8, >= 23) || (9, <= 22) => Zodiac.virgo,
-      (9, >= 23) || (10, <= 22) => Zodiac.libra,
-      (10, >= 23) || (11, <= 21) => Zodiac.scorpio,
-      (11, >= 22) || (12, <= 21) => Zodiac.sagittarius,
+      (8, >= 23) || (9, <= 23) => Zodiac.virgo,
+      (9, >= 24) || (10, <= 22) => Zodiac.libra,
+      (10, >= 23) || (11, <= 22) => Zodiac.scorpio,
+      (11, >= 23) || (12, <= 24) => Zodiac.sagittarius,
       _ => Zodiac.capricorn,
     };
   }
