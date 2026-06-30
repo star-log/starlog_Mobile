4634f0325a7e3aa833fed6b8d76c59c216ece4fb
leejia0324 <hx.un0707@gmail.com>
2026-06-30 17:14:40 +0900

    refactor: app asset 클래스 네이밍 수정

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -1,4 +1,4 @@
-class AppAssets {
+class StarlogAppAssets {
   static const appBackground = 'assets/svg/starlog_background.svg';
   static const splashStar = 'assets/svg/splash_star.svg';
   static const work = 'assets/svg/work_icon.svg';
